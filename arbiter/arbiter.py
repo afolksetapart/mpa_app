eea384f2cfab2384f44c802858634007bab7e412
Joe Michelini <josephmichelini@gmail.com>
2020-10-29 06:32:48 -0400

    config arbiter class

--- arbiter/arbiter.py
+++ arbiter/arbiter.py
@@ -2,17 +2,34 @@ class Arbiter():
     def __init__(self, title, candidate):
         self.title = title
         self.candidate = candidate
+        self.accuracy = 0
 
     @classmethod
-    def determine(cls, title, candidate):
+    def compare(cls, title, candidate):
         arbiter = cls(title=title, candidate=candidate)
         title = arbiter.title
         candidate = arbiter.candidate
-        accuracy = 0
 
-        if title.title_key in candidate.text:
-            accuracy += 1
+        if "Sheet music" in candidate.labels:
+            if title.title_key in candidate.text:
+                arbiter.accuracy += 1
 
-        for key in title.from_keys:
-            if key in candidate.text:
-                accuracy += 1
+            for key in title.from_keys:
+                if key in candidate.text:
+                    arbiter.accuracy += .5
+
+            if "disney" in candidate.text:
+                arbiter.accuracy += .5
+
+            if "medley" in candidate.text:
+                pass
+
+        else:
+            pass
+        # print "no match"
+
+    def determine(self):
+        if self.accuracy >= 1:
+            return True
+        else:
+            return False
